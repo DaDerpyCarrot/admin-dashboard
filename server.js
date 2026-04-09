f4f56a88f8370a7234954fc4d7b3c6113fb4b24d
DaDerpyCarrot <daderpycarrot9@gmail.com>
2026-04-10 00:32:12 +0800

    updated server

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors({
   origin: [
-    "https://roadimentary-dashboard.onrender.com"
+    "https://roadimentary-dashboard.onrender.com",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500"
   ]
 }));
 app.use(express.json());
