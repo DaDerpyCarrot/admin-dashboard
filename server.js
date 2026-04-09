c9826e7c3002fad6046781e1483abc58afb4b8fa
DaDerpyCarrot <daderpycarrot9@gmail.com>
2026-04-09 22:05:31 +0800

    update server

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://roadimentary-dashboard.onrender.com"
+  ]
+}));
 app.use(express.json());
 
 app.get("/api/health", (req, res) => {
